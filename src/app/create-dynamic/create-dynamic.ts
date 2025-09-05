4c807dc388cfac7afe9aec03b7a51a0fb16ecccf
Vasiliy <dvv03122014@gmail.com>
2025-09-05 19:57:54 +0300

    v20 two-way binding

--- src/app/create-dynamic/create-dynamic.ts
+++ src/app/create-dynamic/create-dynamic.ts
@@ -1,10 +1,21 @@
-import {Component, ComponentRef, effect, signal, viewChild, ViewContainerRef} from '@angular/core';
+import {
+  Component,
+  ComponentRef,
+  inputBinding,
+  outputBinding,
+  signal,
+  twoWayBinding,
+  viewChild,
+  ViewContainerRef
+} from '@angular/core';
 import {BeCreatedDynamically} from './be-created-dynamically/be-created-dynamically';
+import { MatTooltip } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-create-dynamic',
   templateUrl: './create-dynamic.html',
-  styleUrl: './create-dynamic.scss'
+  styleUrl: './create-dynamic.scss',
+  imports: [MatTooltip]
 })
 export class CreateDynamic {
 
@@ -12,27 +23,34 @@ export class CreateDynamic {
   compactMode = signal(false);
   #componentRef?: ComponentRef<BeCreatedDynamically>;
 
-  constructor() {
-    effect(() => {
-      const isCompactMode = this.compactMode();
-      this.#componentRef?.setInput('collapsed', isCompactMode);
-      // this.#componentRef?.setInput('collapsed', this.compactMode());  won't work, won't be tracked by effect !
-    })
-  }
+  constructor() {}
 
   createComponent() {
     this.vcr()?.clear();
 
-    this.#componentRef = this.vcr()!.createComponent(BeCreatedDynamically);
-
-    this.#componentRef?.setInput('collapsed', this.compactMode());
-    this.#componentRef?.setInput('description', 'description');
-    this.#componentRef?.setInput('collapsed', this.compactMode());
-
-    this.#componentRef?.instance.collapsed.subscribe((isCollapsed) => {
-      console.log('CHANGED___________', isCollapsed);
-      this.compactMode.set(isCollapsed); // two way binding
-    })
+    this.#componentRef = this.vcr()!.createComponent(BeCreatedDynamically, {
+      bindings: [
+        inputBinding('title', () => 'Weather'),
+        inputBinding('description', () => 'description'),
+        // inputBinding('collapsed', () => this.compactMode()), // more complicated, use next line
+        // inputBinding('collapsed', this.compactMode),
+        // outputBinding<boolean>('collapsedChange', (isCollapsed: boolean) => this.compactMode.set(isCollapsed)),
+        twoWayBinding('collapsed', this.compactMode), // instead of input + output
+        outputBinding('closed', (closed) => {
+          this.#componentRef?.destroy();
+          this.#componentRef = undefined;
+        })
+      ],
+      directives: [
+        {
+          type: MatTooltip,
+          bindings: [
+            inputBinding('matTooltip', () => 'Tooltip'),
+            inputBinding('matTooltipPosition', () => 'above'),
+          ]
+        }
+      ]
+    });
   }
 
   setCollapsed(isCollapsed: boolean) {
